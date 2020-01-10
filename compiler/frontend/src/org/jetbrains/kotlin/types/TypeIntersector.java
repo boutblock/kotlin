564346398455ba7f9692ca1708ceec833c2d8103
Denis Zharkov <denis.zharkov@jetbrains.com>
2020-01-10 19:30:37 +0300

    Fix nullability of intersection of one of the type is generic parameter
    Otherwise org.jetbrains.kotlin.spec.checkers.DiagnosticsTestSpecGenerated.Linked.Type_system.Introduction_1.P_6.Neg#test2_3
    starts failing once we start checking subtyping of intersection-type
    at org.jetbrains.kotlin.resolve.calls.smartcasts.SmartCastManager#checkAndRecordPossibleCast

--- compiler/frontend/src/org/jetbrains/kotlin/types/TypeIntersector.java
+++ compiler/frontend/src/org/jetbrains/kotlin/types/TypeIntersector.java
@@ -58,7 +58,7 @@ public class TypeIntersector {
             if (KotlinBuiltIns.isNothingOrNullableNothing(type)) {
                 nothingOrNullableNothing = type;
             }
-            allNullable &= type.isMarkedNullable();
+            allNullable &= TypeUtils.isNullableType(type);
             nullabilityStripped.add(TypeUtils.makeNotNullable(type));
         }
 
